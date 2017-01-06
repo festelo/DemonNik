f6558bd993bbd4f6b9c6a8e662c7df0d5bd92f4e
Igor <igor.spiridonov.99@mail.ru>
2017-01-07 02:27:00 +0300

    who reads it?

--- sample/Controller.java
+++ sample/Controller.java
@@ -10,7 +10,11 @@ import javafx.scene.control.ToggleButton;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import static java.lang.Thread.sleep;
+
 public class Controller implements Initializable {
+
+    final int MAX_ENERGY_PER_TIME = 1000;
 
     @FXML
     private ChoiceBox amountOfTeapots;
@@ -34,10 +38,40 @@ public class Controller implements Initializable {
     private Label stateOfComputers;
 
     @FXML
-    private ToggleButton btn;
+    private ToggleButton counter;
 
-    public void pressCounter() {
+    public void pressCounter() throws InterruptedException {
+        int int_amountOfTeapots = Integer.parseInt((String) amountOfTeapots.getValue());
+        int int_energyForTeapot = Integer.parseInt(energyForTeapot.getText());
+        int energyForAllTeapots = int_amountOfTeapots  * int_energyForTeapot;
 
+        int int_amountOfBulbs = Integer.parseInt((String) amountOfBulbs.getValue());
+        int int_energyForBulb = Integer.parseInt(energyForBulb.getText());
+        int energyForAllBulbs = int_amountOfBulbs  * int_energyForBulb;
+
+        int int_amountOfComputers = Integer.parseInt((String) amountOfComputers.getValue());
+        int int_energyForComputer = Integer.parseInt(energyForComputer.getText());
+        int energyForAllComputers = int_amountOfComputers  * int_energyForComputer;
+
+        int energyPerTime = energyForAllTeapots + energyForAllBulbs + energyForAllComputers;
+
+        int currentEnergy = 0;
+
+        if (energyPerTime > MAX_ENERGY_PER_TIME) {
+            counter.setText("НЕ РАБОТАЕТ");
+            counter.setSelected(false);
+            return;
+        } else {
+            while(true) {
+                currentEnergy = currentEnergy + energyPerTime;
+                System.out.println(String.valueOf((int)currentEnergy));
+                //counter.setText(String.valueOf((int)currentEnergy));
+                Thread.sleep(5000);
+                if(!counter.isSelected()) {
+                    break;
+                }
+            }
+        }
     }
 
     @Override
