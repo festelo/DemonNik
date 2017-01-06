b88a0813c51cf5e8e50eada2f22e393f29d224a0
Igor <igor.spiridonov.99@mail.ru>
2017-01-07 01:20:38 +0300

    Initial commit

---
+++ sample/Controller.java
@@ -0,0 +1,38 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleButton;
+
+public class Controller {
+
+    @FXML
+    private ChoiceBox count1;
+    @FXML
+    private ChoiceBox count2;
+    @FXML
+    private ChoiceBox count3;
+
+    @FXML
+    private TextField energy1;
+    @FXML
+    private TextField energy2;
+    @FXML
+    private TextField energy3;
+
+    @FXML
+    private Label state1;
+    @FXML
+    private Label state2;
+    @FXML
+    private Label state3;
+
+    @FXML
+    private ToggleButton btn;
+
+    public void press() {
+
+    }
+}
